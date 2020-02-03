b3efa39279304f805459e7c73d192ae68bf3eddf
kareem <kareemadesola1999@gmail.com>
2020-02-03 13:33:29 +0100

    Changed year
    Changed the year from 2019 to 2020 to see if sendgrid will deliver that email but it still didn't

--- success.php
+++ success.php
@@ -18,7 +18,7 @@ if(isset($_POST['submit'])){
   $specialtyName = $crud->getSpecialtiesById($specialty);
 
   if($isSuccess){
-    SendEmail::SendMail($email,'Welcome to IT Conference 2019','You have successfully registered for this year\'s IT Conference');
+    SendEmail::SendMail($email,'Welcome to IT Conference 2020','You have successfully registered for this year\'s IT Conference');
     include 'includes/successMessage.php';
 
   } else{
