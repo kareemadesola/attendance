997ba8416c0a7acda52640e4580858dc479d4102
kareem <kareemadesola1999@gmail.com>
2020-02-06 20:57:45 +0100

    contains api key

--- sendEmail.php
+++ sendEmail.php
@@ -3,7 +3,7 @@
 
     class SendEmail{
         public static function SendMail($to, $subject, $content){
-            $key = '';
+            $key = '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';
 
             $email = new \SendGrid\Mail\Mail();
             $email->setFrom('kareemadesola1999@gmail.com', 'Kareem Adesola');
